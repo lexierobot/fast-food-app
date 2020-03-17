781908d861c67de54d8c055f0be584647d3809a3
lexiehelwig <plumlexie@gmail.com>
2020-03-17 16:24:29 -0400

    Some tweaks to homework

--- menu.js
+++ menu.js
@@ -1,8 +1,9 @@
+//Define Variable customerOrder
 //Add Event Listener to Order Button
 var customerOrder = document.getElementById("submitOrder").addEventListener("click", confirmOrder);
 
 function confirmOrder() {
-    //prevent page reload?
+    //prevent page auto-reload?
     event.preventDefault();
 
     //console log the order details
@@ -12,8 +13,5 @@ function confirmOrder() {
     document.getElementById("orderForm").reset();
 
     //print confirmation message for customer
-    document.getElementById("confirmOrder").innerHTML = "Thanks! We've received your order of... Item: " + document.getElementById("item").value + ", Qty: " + document.getElementById("qty").value;
+    document.getElementById("confirmOrder").innerHTML = "Thanks! We've received your order of Item: " + document.getElementById("item").value + ", Qty: " + document.getElementById("qty").value;
 };
-
-
-// Display Order Details
