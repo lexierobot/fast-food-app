6868b5e6343afcff489687d3d733952e2913b20b
lexiehelwig <plumlexie@gmail.com>
2020-03-17 16:01:27 -0400

    Logs to console twice, so confirmation message prints second log, which is a blank order

--- menu.js
+++ menu.js
@@ -1,3 +1,19 @@
-document.querySelector("#submitOrder").addEventListener('click', placeOrder => {
-    console.log("I've got the order");
-});
+//Add Event Listener to Order Button
+var customerOrder = document.getElementById("submitOrder").addEventListener("click", confirmOrder);
+
+function confirmOrder() {
+    //prevent page reload?
+    event.preventDefault();
+
+    //console log the order details
+    console.log("Order received. Item: " + document.getElementById("item").value + ", Qty: " + document.getElementById("qty").value);
+
+    //reset form
+    document.getElementById("orderForm").reset();
+
+    //print confirmation message for customer
+    document.getElementById("confirmOrder").innerHTML = "Thanks! We've received your order of: " + document.getElementById("item").value + ", Qty: " + document.getElementById("qty").value;
+};
+
+
+// Display Order Details
