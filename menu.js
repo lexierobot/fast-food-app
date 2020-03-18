0b7e35e13de77485eca12317dad2ec6b7768e614
lexiehelwig <plumlexie@gmail.com>
2020-03-18 15:41:23 -0400

    Split, trim, menu object added

--- menu.js
+++ menu.js
@@ -1,9 +1,9 @@
-function order() {
+function order(){
     //prevent page auto-reload?
     event.preventDefault();
 
     //var customerOrder
-    var customerOrder = "Item: " + document.getElementById("item").value + ", Qty: " + document.getElementById("qty").value;
+    var customerOrder = document.getElementById("item").value;
 
     //log customerOrder
     console.log(customerOrder);
@@ -13,6 +13,33 @@ function order() {
 
     //confirmation message
     document.getElementById("successMessage").innerHTML = "Thanks, we've received your order: <br> " + customerOrder;
+
+    let order = customerOrder.split(',');
+    for (var i = 0; i < order.length; i++) {
+        order[i] = order[i].trim();
+    }
+    console.log(order);
 };
+
+//Menu & Stock Qty Object
+let menu = {
+    //sandwiches
+    'hamburger': 25,
+    'cheeseburger': 14,
+    'grilled chicken': 33,
+    //sides
+    'french fries': 20,
+    'sweet potato fries': 3,
+    'salad': 4,
+    //drinks
+    'coke': 100,
+    'diet coke': 30,
+    'bottled water': 20,
+    //milkshakes
+    'milkshake': 9,
+    'cookie': 80,
+    'candy': 5,
+};
+
 
 
