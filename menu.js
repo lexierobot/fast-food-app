019b5203c3cc4ba851af4819e9dad64993695ab5
lexiehelwig <plumlexie@gmail.com>
2020-03-19 16:16:29 -0400

    Got it to remove all special characters and spaces after splitting my object into an arrayMade cool progress using a regex

--- menu.js
+++ menu.js
@@ -1,38 +1,4 @@
-function order(){
-    //prevent page auto-reload?
-    event.preventDefault();
-
-    //var customerOrder
-    var customerOrder = document.getElementById("item").value;
-
-    //log customerOrder
-    console.log(customerOrder);
-
-    //reset form
-    document.getElementById("orderForm").reset();
-
-    //confirmation message
-    document.getElementById("successMessage").innerHTML = "Thanks, we've received your order: <br> " + customerOrder;
-
-    //split string at commas, then trim
-    let splitOrder = customerOrder.split(',')
-    ;
-    for (var i = 0; i < splitOrder.length; i++) {
-        splitOrder[i] = splitOrder[i].trim();
-    }
-    //check
-    console.log(splitOrder);
-
-      //split & trim again
-      for (x in splitOrder) {
-        console.log(splitOrder[x].split(':'));
-        console.log(splitOrder[x].trim());
-        }
-    };
-
-  
-
-//Menu & Stock Qty Object
+//Menu Object
 let menu = {
     //sandwiches
     'hamburger': 25,
@@ -50,4 +16,32 @@ let menu = {
     'milkshake': 9,
     'cookie': 80,
     'candy': 5,
-};
+}
+
+function order(){
+    //prevent page auto-reload?
+    event.preventDefault();
+
+    //var customerOrder
+    var customerOrder = document.getElementById("item").value;
+
+    //log customerOrder
+    console.log(customerOrder);
+
+    //reset form
+    document.getElementById("orderForm").reset();
+
+    //confirmation message
+    document.getElementById("successMessage").innerHTML = "Thanks, we've received your order: <br> " + customerOrder;
+
+    //split string at commas, then colon
+    let splitOrder = customerOrder.split(',');
+       for (var i = 0; i <splitOrder.length; i++) {
+           splitOrder[i] = splitOrder[i].replace(/[&\/\\#,+()$~%.'"*?<>{} ]/g, '');
+       }
+
+        console.log(splitOrder);
+    };
+
+ 
+
