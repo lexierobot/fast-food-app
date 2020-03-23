aee9c8550f9abed4478de34f3a6e401b74f8e4c4
lexiehelwig <plumlexie@gmail.com>
2020-03-23 17:13:44 -0400

    Able to loop through object but not successfully comparing input

--- menu.js
+++ menu.js
@@ -1,37 +1,29 @@
 //Menu Object
 let menu = {
-    //sandwiches
-    'hamburger': 25,
-    'cheeseburger': 14,
-    'grilled chicken': 33,
-    //sides
-    'french fries': 20,
-    'sweet potato fries': 3,
-    'salad': 4,
-    //drinksd
-    'coke': 100,
-    'diet coke': 30,
-    'bottled water': 20,
-    //milkshakes
-    'milkshake': 9,
-    'cookie': 80,
-    'candy': 5,
+    hamburger: 25,
+    cheeseburger: 14,
+    chicken: 33,
+    fries: 20,
+    tots: 3,
+    salad: 4,
+    coke: 100,
+    tea: 30,
+    water: 20,
+    milkshake: 9,
+    cookie: 80,
+    candy: 5
 };
 
 function order(){
     //prevent page auto-reload?
     event.preventDefault();
-
     //var customerOrder
     var customerOrder = document.getElementById("item").value;
-
-    //log customerOrder
+    //log user input to console test
     console.log(customerOrder);
-
-    //reset form
+    //reset order form
     document.getElementById("orderForm").reset();
-
-    //confirmation message
+    //display order receipt to customer
     document.getElementById("successMessage").innerHTML = "Thanks, we've received your order: <br> " + customerOrder;
 
     //split string at commas, then colon
@@ -39,29 +31,18 @@ function order(){
     for (let i = 0; i <splitOrder.length; i++) {
         let splitOrderAgain = splitOrder[i].split(":");
 
-        //check
-        console.log(splitOrderAgain);
-
         //trim order
         for (let j = 0; j < splitOrderAgain.length; j++) {
             let trimOrder = splitOrderAgain[j].trim();
 
-            //remove special characters
+            //remove special characters just for regex
             let newOrder = trimOrder.replace(/[^a-zA-Z0-9: ]/g, '');
 
-            //check input
-            console.log(newOrder);
-
-            for(let k in menu) {
-                function checkItem(item) {
-                if(menu[k].properties === newOrder){
-                    console.log (newOrder + "exists")
-                } else {
-                    console.log (newOrder + " does not exist");
-                }
-            }} checkItem();
-
-                    // console.log(`${property}: ${menu[property]}`);
+        //check order input against menu items
+        for(let x in menu) {
+            if(x != newOrder){
+                console.log(newOrder + " is not on the menu.")
+            }
         }
     }
-};
+}};
